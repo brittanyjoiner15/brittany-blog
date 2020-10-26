375e8d647943be5db9d139e11f294c751e5a4cd0
Brittany Joiner <brittanyjoiner15@gmail.com>
2020-10-26 13:31:09 -0500

    adding home page

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from "react"
+import { Link, graphql } from "gatsby"
+
+const Home = () => {
+  return (
+    <div>
+     <p>Hi</p>
+     <a href="/blog">View my blog</a>
+    </div>
+  )
+}
+
+export default Home
