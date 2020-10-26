d64c470530b01802841a779f983a98122de9c6fe
Brittany Joiner <brittanyjoiner15@gmail.com>
2020-10-26 13:37:41 -0500

    added EUI

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,15 @@
 import React from "react"
 import { Link, graphql } from "gatsby"
+import '@elastic/eui/dist/eui_theme_light.css';
+import {EuiButton} from "@elastic/eui";
+ 
+
 
 const Home = () => {
   return (
     <div>
      <p>Hi</p>
-     <a href="/blog">View my blog</a>
+     <EuiButton href="/blog">View my blog</EuiButton>
     </div>
   )
 }
